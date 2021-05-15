e85ec43aef91bef54347a2f87ff0c0ee8eaa5b59
Oscar O'Neill <theoscaroneill@aol.com>
2021-05-15 13:21:34 -0400

    component updates

--- src/routes/Dawn.js
+++ src/routes/Dawn.js
@@ -5,7 +5,6 @@ import dawn from '../media/music/08-dawn-outerlude.mp3';
 
 const Dawn = () => {
     document.title = "dawn";
-    const path = window.location.pathname;
     const background = "url(https://firebasestorage.googleapis.com/v0/b/yesterday-data.appspot.com/o/images%2Fbackground%2Fsimon-shim-LX7f4K8t9o4-unsplash.jpg?alt=media&token=3310b46a-82d7-4e73-a6db-74072016c0d3)";
 
     return (
--- src/routes/NY1.js
+++ src/routes/NY1.js
@@ -7,7 +7,6 @@ import Song from '../components/Song';
 const NY1 = () => {
     document.title = "ny1 (new york minute), pt. 2";
     const background = "url(https://firebasestorage.googleapis.com/v0/b/yesterday-data.appspot.com/o/images%2Fbackground%2Fivana-cajina-qpNhPW7zyFg-unsplash.jpg?alt=media&token=b28526bd-aebe-4980-8858-a3c04eb5d386)";
-    const path = window.location.pathname;
 
     return (
         <>
